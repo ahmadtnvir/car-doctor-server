56f344c06d0676ebc80c82020cdfb9cc886ec2ff
Tanvir Ahmad <tnvir9@gmail.com>
2024-05-10 17:44:38 +0600

    Add data to database through post method

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  })
+)
 app.use(express.json());
 
 
@@ -23,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const carsCollection = client.db('carDoctorDB').collection("services");
@@ -46,14 +54,15 @@ async function run() {
       res.send(result);
     })
 
-    get.post('/booking', async(req, res) => {
+    app.post('/bookings', async(req, res) => {
       const booking = req.body;
+      console.log(booking);
       const result = await bookingsCollection.insertOne(booking);
       res.send(result)
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
